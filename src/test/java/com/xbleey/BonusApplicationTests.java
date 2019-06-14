3592b95c65b4d50b76cb07d0a6600a2d189e78aa
xbleey <xbleey@126.com>
2019-06-14 14:45:39 +0800

    v1.0 1.创建项目

---
+++ src/test/java/com/xbleey/BonusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xbleey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BonusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
